1db24f266ba2d1149cb4c25683603353a9c66287
Amabuh Fru <amafru@hotmail.com>
2018-11-07 10:22:44 +0000

    first jenkins CI build

---
+++ JenkinsFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+
+        public static string createMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ JenkinsFirstTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsFirst;
+
+namespace JenkinsFirstTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.createMessage());
+        }
+    }
+}
